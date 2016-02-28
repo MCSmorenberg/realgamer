73eb8f015e4d514a8f8623712bdf7755dc40848f
Maurice <mcsmorenberg@gmail.com>
2016-02-28 16:18:54 +0100

    Puma, Cors and config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,14 @@
-class TodoApi::Application
-   config.middleware.insert_before 0,  "Rack::Cors", debug: Rails.env.development?, logger: (-> { Rails.logger }) do
+class Realgamer::Application
+  config.middleware.insert_before 0,  "Rack::Cors", debug: true, logger: (-> { Rails.logger }) do
      allow do
-       origins 'localhost:3000', '127.0.0.1:3000',
-         /\Ahttp:\/\/192\.168\.0\.\d{1,3}(:\d)?\z/
- 
+       origins 'localhost:3000'
+
        resource '/cors',
          headers: :any,
          methods: [:post],
          credentials: true,
          max_age: 0
- 
+
        resource '*',
          headers: :any,
          methods: [:get, :post, :delete, :put, :patch, :options, :head],
