4e5d482f8b2c8751819cfdac4828fe9bd7fe8c93
Maurice <mcsmorenberg@gmail.com>
2016-03-02 11:26:43 +0100

    Factories and test review test adjustments

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -1,32 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsController, type: :controller do
-  let!(:a_review) {Review.create! valid_attributes}
-
-  let(:valid_attributes) do
-    {
-      game: 1,
-      name: "Mr. Tests",
-      description: "Write rspec tests for the review controller",
-      rating: 5
-    }
-  end
-
-  let(:invalid_attributes) do
-    {
-      name: ""
-    }
-  end
+  review = FactoryGirl.create(:review)
+  game = FactoryGirl.create(:game)
 
   describe "Get #index" do
    it "returns a 200" do
-     get :index
+     get :index, :game_id => game.id
      expect(response).to be_success
     end
   end
 
   it "returns json" do
-    get :index
+    get :index, :game_id => game.id
     expect(response.header['Content-Type']).to include('application/json')
   end
 
@@ -34,7 +20,7 @@ RSpec.describe ReviewsController, type: :controller do
      context "with valid params" do
        it "creates a new review" do
            expect {
-             post :create, {review: valid_attributes}
+             post :create, review: FactoryGirl.attributes_for(:review), :game_id => game.id
            }.to change(Review, :count).by(1)
        end
      end
---
+++ spec/factories/games.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :game do
+    title "Testgame"
+    genre "Testing"
+    developer "Testers"
+    publisher "Testing Company"
+    release_date "a date"
+    slipcover "Some img"
+    description "It's all about testing"
+  end
+end
